318b092a9e32fa6b873270e64922504f489fffde
hectormateos <hectormateosoblanca@gmail.com>
2018-05-21 19:05:10 +0200

    git guard apikey firebase

--- src/modules/monkeys/monkeys.service.ts
+++ src/modules/monkeys/monkeys.service.ts
@@ -8,7 +8,6 @@ export class MonkeysService {
     firebaseApp: any;
     monkeys: any;
     config = {
-        apiKey: 'AIzaSyD-ASOBSi_cDbaEPM1Hy2N7X97H4rBykug',
         databaseURL: 'https://nest-01-db.firebaseio.com',
         projectId: 'nest-01-db',
     };
