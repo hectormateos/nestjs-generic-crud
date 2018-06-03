6dd432e180f7b044803ebef4f1ef95106d7eec94
hectormateos <hectormateosoblanca@gmail.com>
2018-06-03 12:16:45 +0200

    users

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,24 +1,24 @@
-import { Entity, Column } from 'typeorm';
-import { BaseEntity } from '../../base/base.entity';
-import { ApiModelProperty } from '@nestjs/swagger';
+import {Entity, Column} from 'typeorm';
+import {BaseEntity} from '../../base/base.entity';
+import {ApiModelProperty} from '@nestjs/swagger';
 
 @Entity('users')
-export class User extends BaseEntity{
+export class User extends BaseEntity {
 
-  @Column({ length: 50, default: 'default-name' })
-  @ApiModelProperty()
-  readonly name: string;
+    @Column({length: 50})
+    @ApiModelProperty()
+    readonly name: string;
 
-  @Column({ length: 50, default: 0 })
-  @ApiModelProperty()
-  readonly age: number;
-  
-  @Column({ length: 50, default: 'none' })
-  @ApiModelProperty()
-  readonly favouriteColor: string;
+    @Column()
+    @ApiModelProperty()
+    readonly age: number;
 
-  constructor(o: Object) {
-    super();
-    Object.assign(this, o);
-  }
+    @Column({length: 50})
+    @ApiModelProperty()
+    readonly favouriteColor: string;
+
+    constructor(o: Object) {
+        super();
+        Object.assign(this, o);
+    }
 }
